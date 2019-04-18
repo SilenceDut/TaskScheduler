f60670193e6aceb41f581c9f4a3faa2a80b18367
SilenceDut <silencedutchman@foxmail.com>
2019-04-18 16:48:22 +0800

    使用Android 平台的接口设置线程优先级

--- taskscheduler/src/main/java/com/silencedut/taskscheduler/ThreadFactory.java
+++ taskscheduler/src/main/java/com/silencedut/taskscheduler/ThreadFactory.java
@@ -15,7 +15,6 @@ class ThreadFactory {
         BackgroundRunnable(Runnable runnable) {
             this.runnable = runnable;
         }
-
         @Override
         public void run() {
             Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
